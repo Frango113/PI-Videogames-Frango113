4838bcec332162afab47c1db41ed6a48f9e33244
Frango113 <frangoat38@aol.com>
2023-09-28 11:49:38 -0300

    9/28

---
+++ api/src/routes/genres.js
@@ -0,0 +1,14 @@
+const { Router } = require("express");
+const { Genre } = require("../db");
+const router = Router();
+
+router.get("/", async (req, res) => {
+  try {
+    const vgGenres = await Genre.findAll();
+    res.status(200).json(vgGenres);
+  } catch (error) {
+    res.send(`Error in route /genres ${error}`);
+  }
+});
+
+module.exports = router;
