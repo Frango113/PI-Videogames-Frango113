a75717b6de63e42688570f27129b6a1894aee3d9
Frango113 <frangoat38@aol.com>
2023-10-09 14:06:42 -0300

    9/10

--- client/src/Views/Home/Home.jsx
+++ client/src/Views/Home/Home.jsx
@@ -1,14 +1,57 @@
-function Detail() {
-  useEffect(() => {
-    return () => {
-      clearState();
-    };
-  });
-  return (
-    <div>
-      <h1>Detail</h1>
-    </div>
-  );
-}
+import { useEffect } from "react";
+import { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { Link } from "react-router-dom";
+import card from "../../Components/Card/Card";
+import Paging from "../../Components/Paging/Paging";
 
-export default Detail;
+import genrefilter from "../../redux/Actions/genrefilter/index";
+import vgorigin from "../../redux/Actions/vgorigin/index";
+import sortvgames from "../../redux/Actions/sortvgame/index";
+import SearchBar from "../../Components/nav/navbar";
+import getGenres from "../../redux/Actions/getgenres";
+import { getVideogames } from "../../redux/Actions/getvgames";
+import deletegame from "../../redux/Actions/deletegame";
+
+export default function HomePage() {
+  const dispatch = useDispatch();
+  allVgames = useSelector((state) => state.videogames);
+  const allgenres = useSelector((state) => state.genres);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [vgamesPerPage] = useState(8);
+  const lastVgameIndex = currentPage * vgamesPerPage;
+  const firstVgIndex = lastVgameIndex - vgamesPerPage;
+  const currentVgames = allVgames.slice(firstVgIndex, lastVgameIndex);
+  const [render, setRender] = useState("");
+
+  const actualPage = (pageNumber) => {
+    setCurrentPage(pageNumber);
+  };
+  useEffect(() => {
+    dispatch(getVideogames());
+    dispatch(getGenres());
+  }, [dispatch]);
+
+  function handleDelete(id) {
+    dispatch(deletegame(id));
+    alert("The game has been removed");
+    dispatch(getVideogames());
+  }
+  function handleGenreFilter(e) {
+    e.preventDefault();
+    dispatch(genrefilter(e.target.value));
+  }
+  function handleOriginFilter(e) {
+    dispatch(vgorigin(e.target.value));
+    setCurrentPage(1);
+  }
+  function handleShowAll(e) {
+    dispatch(vgorigin("All"));
+    dispatch(sortvgames("asc"));
+  }
+  function handleSortcgames(e) {
+    e.preventDefault();
+    dispatch(sortvgames(e.target.value));
+    setRender(`Order${e.target.value}`);
+  }
+}
