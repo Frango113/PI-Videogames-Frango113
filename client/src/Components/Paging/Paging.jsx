169d967a99ff841a8a929af45fa401d71c475040
Frango113 <frangoat38@aol.com>
2023-10-12 11:22:38 -0300

    rehaciendo esta pinchila

--- client/src/Components/Paging/Paging.jsx
+++ client/src/Components/Paging/Paging.jsx
@@ -1,31 +1,69 @@
 import React from "react";
-import stl from "./Paging.module.css";
-export default function Paging({
-  vgamesPerPage,
-  allVgames,
-  currpage,
-  actualPage,
-}) {
-  const pageNumbers = [];
-  const maxpage = Math.ceil(allVgames / vgamesPerPage);
+import stlye from "./Paging.module.css";
+import { useDispatch, useSelector } from "react-redux";
 
-  for (let i = 0; i < maxpage; i++) {
-    pageNumbers.push(i + 1);
-  }
+const Paging = () => {
+  const dispatch = useDispatch;
+  const pages = useSelector((state) => state.pages);
+  const currentPage = useSelector((state) => state.currentPage);
+
+  const handleValue = (value) => {
+    dispatch(paginado(value));
+  };
+  const nextImg = "https://cdn-icons-png.flaticon.com/128/10696/10696041.png";
+  const prevImg = "https://cdn-icons-png.flaticon.com/128/10696/10696056.png";
+  const startImg = "https://cdn-icons-png.flaticon.com/128/32/32766.png";
+  const endImg = "https://cdn-icons-png.flaticon.com/128/32/32738.png";
 
   return (
-    <nav>
-      <ul className={stl.pagination}>
-        {pageNumbers &&
-          pageNumbers.map((num) => {
-            return (
-              <li className={stl.pagenr} key={num}>
-                <button onClick={() => actualPage(num)}>{num}</button>
-              </li>
-            );
-          })}
-        <span>{`   Actual Page  ${currpage}`}</span>
-      </ul>
-    </nav>
+    <div className={StylePropertyMap.globalCont}>
+      <div className={StylePropertyMap.divCont}>
+        <button
+          className={StylePropertyMap.prev}
+          onClick={() => handleValue("start")}
+        >
+          <img
+            title="start"
+            src={startImg}
+            atl={"prev"}
+            className={StylePropertyMap.imgPrev}
+          />
+        </button>
+        <button className={style.prev} onClick={() => handleValue("prev")}>
+          <img
+            title="prev"
+            src={prevImg}
+            alt={"prev"}
+            className={style.imgPrev}
+          />
+        </button>
+        {pages.map((n) => {
+          return (
+            <button
+              className={style.num}
+              style={{ color: currentPage === n ? "white" : "black" }}
+              onClick={() => handleValue(n)}
+            >
+              {n}
+            </button>
+          );
+        })}
+        <button className={style.next} onCLick={() => handleValue("next")}>
+          <img
+            title="next"
+            src={nextImg}
+            alt={"next"}
+            className={style.imgNext}
+          />
+        </button>
+        <button>
+          {" "}
+          className={style.end} onClick={() => handleValue("end")}
+          <img title="end" src={endImg} alt={"end"} className={style.imgEnd} />
+        </button>
+      </div>
+    </div>
   );
-}
+};
+
+export default Paging;
